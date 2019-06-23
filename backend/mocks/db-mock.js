188e59b8418769e509ea685edd00f8c24f7f626d
WizarBr <florentino.flavioweb@gmail.com>
2019-06-23 20:58:03 -0300

    rotas de usuarios mockados

---
+++ backend/mocks/db-mock.js
@@ -0,0 +1,24 @@
+const USERS = [
+    {
+        id: 1,
+        usarname: 'Flávio Maran Florentino',
+        userlogin: 'fflorentino',
+    },
+    {
+        id: 2,
+        usarname: 'Fabio Maran Florentino',
+        userlogin: 'fflorentino',
+    },
+    {
+        id: 3,
+        usarname: 'Flávio Maran Florentino',
+        userlogin: 'fflorentino',
+    },
+    {
+        id: 4,
+        usarname: 'Flávio Maran Florentino',
+        userlogin: 'fflorentino',
+    }
+];
+
+module.exports = USERS;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const USERS = require('./../mocks/db-mock');
 
-router.get("", (req, res, next) => {
+router.get('', (req, res, next) => {
 
     res.status(200).json({
         message: "users fetched successfully!",
+        payload: USERS
     });
+
+});
+
+router.get('/:id', (req, res, next) => {
+    const id = Number(req.params['id']);
+    const user = USERS.filter(user => user.id === id);
+    console.log('here', user);
+
+    res.status(200)
+        .json({
+            message: 'User fetched successfully',
+            payload: user
+        });
+
 });
 
 module.exports = router;
