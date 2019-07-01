abfd4eb67b646af6c875d00f48a558b5b9586de4
WizarBr <florentino.flavioweb@gmail.com>
2019-07-01 18:39:54 -0300

    included user operations at - user.service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
+import { User } from '../interfaces/user';
 
 
 @Injectable({
@@ -12,11 +13,32 @@ export class UsersService {
 
   constructor(private http: HttpClient) { }
 
-  getAllUsers(): Observable<any> {
+  getAllUsers(): Observable<User[]> {
     return this.http
       .get<any>('http://localhost:3000/api/users')
       .pipe(
         map(res => res['payload'])
       );
+  }
+
+  getUserById(userId: number): Observable<User> {
+    return this.http
+      .get(`http://localhost:3000/api/users/${userId}`)
+      .pipe(
+        map(res => res['payload'])
+      );
+  }
+
+  editUser(user: User): Observable<User> {
+    return this.http
+      .put(`http://localhost:3000/api/users`, user)
+      .pipe(
+        map(res => res['payload'])
+      );
+  }
+
+  deleterUser(userId: number): Observable<any> {
+    return this.http
+      .delete(`http://localhost:3000/api/users/delete/${userId}`);
   }
 }
